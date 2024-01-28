ab3d263b450e4d63109635d02f900772c9d68f4e
Marian-Aziz <153827166+Marian-Aziz@users.noreply.github.com>
2024-01-28 17:01:05 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+//nav.
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
+
+
+
+
+  
+   
